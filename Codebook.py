f80f41e3e45155ee573f448f47d753203f6a5115
Alexander Kalinin <alex@kalinin.uk>
2018-02-25 23:04:56 +0000

    Fixed Codebook init, only show output when not training

--- Codebook.py
+++ Codebook.py
@@ -18,33 +18,31 @@ class Codeword:
                 self.max = val
 
     def checkValue(self, val):
-        print str(self.min) + "," + str(self.max) + "," + str(val)
         if val < self.min or val > self.max:
             return False
         else:
             return True
 
 
+
 class Codebook:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.codes = np.full((width, height), Codeword())
+        self.codes = [[Codeword() for x in range(height)] for y in range(width)]
 
     def addFrame(self, frame):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         for x in range(self.width):
             for y in range(self.height):
-                # print frame.shape
-                # print str(x) + "," + str(y)
                 # use hue
-                self.codes[x][y].addValue(frame[y][x][2])
+                self.codes[x][y].addValue(frame[y][x][0])
 
     def processFrame(self,frame):
-        out = np.zeros((self.height,self.width),np.uint8)
+        out = np.zeros((self.height,self.width),np.uint8) + 255
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         for x in range(self.width):
             for y in range(self.height):
-                if self.codes[x][y].checkValue(frame[y][x][2]):
-                    out[y][x] = 100
+                if self.codes[x][y].checkValue(frame[y][x][0]):
+                    out[y][x] = 0
         return out
--- main.py
+++ main.py
@@ -7,33 +7,23 @@ FRAME_DELAY = 100
 
 
 def main():
-    # c = Codebook(3,3)
-    # for i in c.codes.flatten():
-    #     print i.min
-
-
-    # bg = cv2.imread('background.jpg')
-    # fg = cv2.imread('foreground_standing.jpg')
-    # print bg.shape
-    # height,width,channels = bg.shape
     c = Codebook(320,240)
     # c.addFrame(bg)
     cap = cv2.VideoCapture('walk.mp4')
     #loop through video
-    frames = 0
+    currentFrame = 0
 
     while True:
         ret, frame = cap.read()
         if ret == True:
-            #process current frame
-            if frames < 20:
+            if currentFrame < 50:#num frames to process
+                #used for initial training of codebook
                 c.addFrame(frame)
-                frames += 1
-                # first = False
             else:
-                frame = c.processFrame(frame)
-            cv2.imshow('fgmask',frame)
-            if cv2.waitKey(FRAME_DELAY) & 0xFF == ord('q'):
+                cb_out = c.processFrame(frame)
+                cv2.imshow('fgmask',cb_out)
+            currentFrame += 1
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
             break
